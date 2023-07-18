d09fec91c2457321d5bab81dac040b35cb06bd65
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-18 13:45:01 +0600

    backend initial structure and connected to the db and intial server done

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
-// dependencies
-const http = require('http');
-// app object - module scaffolding
-const app = {};
-// configuration
-app.config = {};
+/* eslint-disable no-tabs */
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+// config setup
+dotenv.config();
+
+// PORT
+const PORT = process.env.PORT || 8080;
+
+// app setup
+const app = express();
+
+// default middleware
+app.use(express.json());
+app.use(cors());
+
+// default routes
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello From House Hunter Server!!' });
+});
+
+// check Health routes
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: 'server health is good!!' });
+});
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zzrczzq.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // Send a ping to confirm a successful connection
+        await client.db('admin').command({ ping: 1 });
+        console.log('Pinged your deployment. You successfully connected to MongoDB!');
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+// server listen and PORT
+app.listen(PORT, () => {
+    console.log('Server is running on PORT 8080');
+});
